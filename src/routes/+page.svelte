267de84e127fe6567134694f96a399244c7d5e08
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-07-14 11:46:27 -0400

    INitial checkin with Sveltekit and Tauri working

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<button on:click={() => console.log('test')}>Click me</button>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
