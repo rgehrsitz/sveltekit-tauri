db0946a17a9d8b1da6dee8fab6f810fd2b4afee6
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-07-14 12:11:39 -0400

    Added Flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { Button } from 'flowbite-svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<button on:click={() => console.log('test')}>Click me</button>
+<Button on:click={() => console.log('test')}>Click me</Button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
