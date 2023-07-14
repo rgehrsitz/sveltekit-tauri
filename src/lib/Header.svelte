7cec49863bebdb5394b83d300017e2561ee1da72
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-07-14 15:16:02 -0400

    Dark mode added

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,8 +1,12 @@
 <script>
 	// Add your header logic here
+	import { DarkMode } from 'flowbite-svelte';
+	let darkmodebtn =
+		'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5 fixed right-2 top-12  md:top-3 md:right-2 z-50';
 </script>
 
 <header class="p-4 bg-blue-500 text-white">
 	<!-- Add your header content here -->
-	<h1>My Application</h1>
+	<h1>Rack Track</h1>
+	<DarkMode btnClass={darkmodebtn} />
 </header>
